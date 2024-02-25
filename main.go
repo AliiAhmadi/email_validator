1a44c031741b4e2a20fa00520ef5ac00433f3d5c
AliiAhmadi <aliahmadi.github@gmail.com>
2024-02-25 19:37:19 +0330

    + add executabe to .gitignore + parse flags function

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	inp, out := args()
+	fmt.Println(*inp, *out)
+}
+
+func args() (*string, *string) {
+	inp := flag.String("i", "input.txt", "The name of input file that contains the emails you want to validate")
+	out := flag.String("o", "output.txt", "The name of output file that want to see result")
+
+	flag.Parse()
+	return inp, out
 }
