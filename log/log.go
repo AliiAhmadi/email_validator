3378a1145b68e8ee6d602b1f4d7af4a66ce3cf76
AliiAhmadi <aliahmadi.github@gmail.com>
2024-02-26 08:58:37 +0330

    + add comment for log functions

--- log/log.go
+++ log/log.go
@@ -7,13 +7,22 @@ import (
 
 type Logger struct{}
 
+// New used for generate new logger instance of Logger struct
 func New() *Logger {
 	return &Logger{}
 }
 
-func (lg *Logger) Info(err error)  {}
+// Info will show information messages in stdout
+func (lg *Logger) Info(err error) {}
+
+// Fatal will show an error and exit with not success
+// status code
 func (lg *Logger) Fatal(err error) {}
+
+// Warning
 func (lg *Logger) Warning(err error) {
 	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 }
+
+// Error
 func (lg *Logger) Error(err error) {}
